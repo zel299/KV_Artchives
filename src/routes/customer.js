9abfdd39bc32e9a1efe7eda53be866865158a8a8
Ruzzel Bernardo <ruzzel29bernardo@gmail.com>
2026-09-10 14:25:07 +0800

    Add checkout with atomic stock decrement and order creation

--- src/routes/customer.js
+++ src/routes/customer.js
@@ -4,6 +4,7 @@ const router = express.Router();
 const ctrl = require("../controllers/customerController");
 const { requireAuth } = require("../middleware/auth");
 const cartCtrl = require("../controllers/cartController");
+const checkoutCtrl = require("../controllers/checkoutController");
 
 router.get("/", ctrl.home);
 router.get("/shop", ctrl.shop);
@@ -15,13 +16,13 @@ router.get("/contact", ctrl.contact);
 router.get("/faqs", ctrl.faqs);
 router.get("/shipping-info", ctrl.shippinginfo);
 router.get("/privacy-policy", ctrl.privacypolicy);
-router.get("/account", ctrl.accountsettings);
-router.get("/account/orders", ctrl.myorders);
 router.get("/account", requireAuth, ctrl.accountsettings);
 router.get("/account/orders", requireAuth, ctrl.myorders);
 router.get("/cart", cartCtrl.showCart);
 router.post("/cart/add", cartCtrl.addToCart);
 router.post("/cart/:id/update", cartCtrl.updateCartItem);
 router.post("/cart/:id/remove", cartCtrl.removeCartItem);
+router.get("/checkout", requireAuth, checkoutCtrl.showCheckout);
+router.post("/checkout", requireAuth, checkoutCtrl.doCheckout);
 
 module.exports = router;
